2439dc25f096cb5360aff48fc9c303d1cca0b70e
DevanshPaliwal <78657285+DevanshPaliwal@users.noreply.github.com>
2024-04-21 22:12:43 +0530

    Add files via upload

---
+++ project/admin/dashboard.php
@@ -0,0 +1,105 @@
+<?php
+
+include '../components/connect.php';
+
+session_start();
+
+$admin_id = $_SESSION['admin_id'];
+
+if(!isset($admin_id)){
+   header('location:admin_login.php');
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>dashboard</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="../css/admin_style.css">
+
+</head>
+<body>
+
+<?php include '../components/admin_header.php' ?>
+
+<!-- admin dashboard section starts  -->
+
+<section class="dashboard">
+
+   <h1 class="heading">dashboard</h1>
+
+   <div class="box-container">
+
+   <div class="box">
+      <h3>Welcome!</h3>
+      <p><?= $fetch_profile['name']; ?></p>
+   </div>
+
+   <div class="box">
+      <?php
+         $select_posts = $conn->prepare("SELECT * FROM `posts` WHERE admin_id = ?");
+         $select_posts->execute([$admin_id]);
+         $numbers_of_posts = $select_posts->rowCount();
+      ?>
+      <h3><?= $numbers_of_posts; ?></h3>
+      <p>Posts added</p>
+      <a href="add_posts.php" class="btn">add new post</a>
+   </div>
+
+   <div class="box">
+      <?php
+         $select_active_posts = $conn->prepare("SELECT * FROM `posts` WHERE admin_id = ? AND status = ?");
+         $select_active_posts->execute([$admin_id, 'active']);
+         $numbers_of_active_posts = $select_active_posts->rowCount();
+      ?>
+      <h3><?= $numbers_of_active_posts; ?></h3>
+      <p>Active posts</p>
+      <a href="view_posts.php" class="btn">see posts</a>
+   </div>
+
+   
+
+   <div class="box">
+      <?php
+         $select_users = $conn->prepare("SELECT * FROM `users`");
+         $select_users->execute();
+         $numbers_of_users = $select_users->rowCount();
+      ?>
+      <h3><?= $numbers_of_users; ?></h3>
+      <p>User accounts</p>
+      <a href="users_accounts.php" class="btn">see users</a>
+   </div>
+
+   <div class="box">
+      <?php
+         $select_admins = $conn->prepare("SELECT * FROM `admin`");
+         $select_admins->execute();
+         $numbers_of_admins = $select_admins->rowCount();
+      ?>
+      <h3><?= $numbers_of_admins; ?></h3>
+      <p>Admin accounts</p>
+      <a href="admin_accounts.php" class="btn">see admins</a>
+   </div>
+   
+
+   </div>
+
+</section>
+
+<!-- admin dashboard section ends -->
+
+
+<!-- custom js file link  -->
+<script src="../js/admin_script.js"></script>
+
+</body>
+</html>
